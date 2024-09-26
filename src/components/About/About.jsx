f26d88432511d687b5ccc676123913f6ecedb783
ifejika <83151638+ifejika@users.noreply.github.com>
2024-09-26 16:35:49 -0400

    create about me page

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './About.css'
+import theme_pattern from '../../assets/theme_pattern.svg'
+
 const About = () => {
   return (
-    <div>
-
+    <div clasName = "about">
+<div classNmae="about-title">
+  <hi>About me </hi>
+  <img src={theme_pattern} alt= ""/>
+</div>
+<div className= "about-section">
+  <div className = "about-right">
+    <div className="about-para">
+      <p> I am a very expericed sofware devloper </p>
+      <p>my passion for coding has made me greate </p>
+    </div>
+    <div classNmae="about-skils">
+      <div className="about-skill"><p>htlm &css </p><hr style={{width:"50%"}}/></div>
+      <div className="about-skill"><p>htlm &css </p><hr style={{width:"50%"}}/></div>
+      <div className="about-skill"><p>htlm &css </p><hr style={{width:"50%"}}/></div>
+      <div className="about-skill"><p>htlm &css </p><hr style={{width:"50%"}}/></div>
+      <div className="about-skill"><p>htlm &css </p><hr style={{width:"50%"}}/></div>
+    </div>
+  </div>
+</div>
     </div>
   )
 }
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
   return (
     <div className= 'hero'>
 <img src={profile} alt=""/>
-<h1> I'm Daniel Ifejika , I am a software developerbased in Toronto</h1>
+<h1> I'm Daniel Ifejika , I am a software developer based in Toronto</h1>
 <p></p> {/** comback here to edit the file  */}
 <div className="hero-action">
  <div className= "hero-connect">Connect with me </div>
