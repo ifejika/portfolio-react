ec1ee85958e2628d0cabacbb62fed422517599f2
ifejika <83151638+ifejika@users.noreply.github.com>
2024-09-26 18:58:13 -0400

    worked on the service page jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import About from './components/About/About'
+import Services from './components/Services/Services'
 const App = () => {
   return (
     <div>
       <Navbar/>
       <Hero/> {/* app.jsx is what render my component   */}
       <About/>
+      <Services/>
     </div>
   )
 }
--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,21 +1,21 @@
 import React from 'react'
 import './About.css'
-import theme_pattern from '../../assets/theme_pattern.svg'
-import './About.css'
+
+
 const About = () => {
   return (
-    <div clasName = "about">
-<div classNmae="about-title">
-  <hi>About me </hi>
-  <img src={theme_pattern} alt= ""/>
+    <div clasName = 'about'>
+<div className="about-title">
+  <h1>About me </h1>
+ 
 </div>
-<div className= "about-section">
+
   <div className = "about-right">
     <div className="about-para">
       <p> I am a very expericed sofware devloper </p>
       <p>my passion for coding has made me greate </p>
     </div>
-    <div classNmae="about-skils">
+    <div className="about-skils">
       <div className="about-skill"><p>htlm &css </p><hr style={{width:"50%"}}/></div>
       <div className="about-skill"><p>htlm &css </p><hr style={{width:"50%"}}/></div>
       <div className="about-skill"><p>htlm &css </p><hr style={{width:"50%"}}/></div>
@@ -24,7 +24,7 @@ const About = () => {
     </div>
   </div>
 </div>
-    </div>
+    
   )
 }
 
